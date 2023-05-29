26c92bbacf7b5bc65737b8fd8c8799d58a8dd01c
Ek Lipina <87270011+exaterin@users.noreply.github.com>
2023-05-29 22:08:34 +0200

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+python3 extract_articles.py --pages 10
+python3 create_dictinary.py
+python3 diacritization.py --train "True" --data "vesmir_articles10.txt" --input "diacritics-dtest.txt" --dict "dict.txt" --output "std"
